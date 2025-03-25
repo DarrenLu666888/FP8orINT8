0d22707c83c70fb9273be4746eec709c2ce9cdbd
DarrenLu666888 <294504270@qq.com>
2025-03-25 07:19:44 -0700

    fp8 real and sim compare

---
+++ 2_FP8_real_demo.py
@@ -0,0 +1,71 @@
+import torch
+
+import triton
+import triton.language as tl
+
+def is_cuda():
+    return triton.runtime.driver.active.get_current_target().backend == "cuda"
+
+@triton.jit
+def fp8_mul_real_kernel(x_ptr,  # *Pointer* to first input vector.
+               y_ptr,  # *Pointer* to second input vector.
+               output_ptr,  # *Pointer* to output vector.
+               n_elements,  # Size of the vector.
+               BLOCK_SIZE: tl.constexpr,  # Number of elements each program should process.
+               # NOTE: `constexpr` so it can be used as a shape value.
+               ):
+    # There are multiple 'programs' processing different data. We identify which program
+    # we are here:
+    pid = tl.program_id(axis=0)  # We use a 1D launch grid so axis is 0.
+    # This program will process inputs that are offset from the initial data.
+    # For instance, if you had a vector of length 256 and block_size of 64, the programs
+    # would each access the elements [0:64, 64:128, 128:192, 192:256].
+    # Note that offsets is a list of pointers:
+    block_start = pid * BLOCK_SIZE
+    offsets = block_start + tl.arange(0, BLOCK_SIZE)
+    # Create a mask to guard memory operations against out-of-bounds accesses.
+    mask = offsets < n_elements
+    # Load x and y from DRAM, masking out any extra elements in case the input is not a
+    # multiple of the block size.
+    x = tl.load(x_ptr + offsets, mask=mask)
+    y = tl.load(y_ptr + offsets, mask=mask)
+    output = tl.fma(x, y, 0.0)
+    # Write x + y back to DRAM.
+    tl.store(output_ptr + offsets, output, mask=mask)
+
+
+def vecmul(a, b):
+    # Check constraints.
+    assert a.shape == b.shape, "Incompatible dimensions"
+    assert a.is_contiguous(), "Matrix A must be contiguous"
+    c = torch.empty(a.shape, device=a.device, dtype=torch.float16)
+    n_elements = a.numel()
+    grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']), )
+    fp8_mul_real_kernel[grid](a, b, c, n_elements, 256)
+    return c
+
+# test fp8_mul_sim_kernel accuracy
+
+TORCH_HAS_FP8 = hasattr(torch, "float8_e4m3fn")
+if TORCH_HAS_FP8 and is_cuda():
+    torch.manual_seed(0)
+    a = torch.randn((256*4), device='cuda', dtype=torch.float16)
+    b = torch.randn((256*4), device='cuda', dtype=torch.float16)
+    a = a.to(torch.float8_e4m3fn)
+    b = b.to(torch.float8_e4m3fn)
+    # print(f"a={a}")
+    # print(f"b={b}")
+    print(triton.testing.do_bench(lambda: vecmul(a, b), warmup=10, rep=100))
+    # triton_output = vecmul(a, b)
+    # triton_output = triton_output.to(torch.float16)
+    # assert a.dtype == torch.float8_e4m3fn
+    # assert b.dtype == torch.float8_e4m3fn
+    # torch_output = torch.mul(a.to(torch.float16), b.to(torch.float16))
+    # print(f"triton_output_with_fp8_inputs={triton_output}")
+    # print(f"torch_output={torch_output}")
+    # if torch.allclose(triton_output, torch_output, atol=0.125, rtol=0):
+    #     print("✅ Triton and Torch match")
+    # else:
+    #     print("❌ Triton and Torch differ")
+else:
+    print("Skipping the test since torch.float8_e4m3fn is not available.")
--- 2_FP8_sim_demo.py
+++ 2_FP8_sim_demo.py
@@ -1,11 +1,14 @@
 import triton
 import triton.language as tl
 import torch
+import pdb
+
 def is_cuda():
     return triton.runtime.driver.active.get_current_target().backend == "cuda"
 
 @triton.jit
-def fp8_mul_kernel(a_ptr, b_ptr, c_ptr, n_elements, BLOCK_SIZE: tl.constexpr):
+def fp8_mul_sim_kernel(a_ptr, b_ptr, c_ptr, n_elements, BLOCK_SIZE: tl.constexpr):
+    # pdb.set_trace()
     pid = tl.program_id(0)
     block_start = pid * BLOCK_SIZE
     offsets = block_start + tl.arange(0, BLOCK_SIZE)
@@ -25,56 +28,69 @@ def fp8_mul_kernel(a_ptr, b_ptr, c_ptr, n_elements, BLOCK_SIZE: tl.constexpr):
 
     # ==== integer mantissa multiply ====
     mant_mul = mant_a * mant_b  # 8-bit * 8-bit => 16-bit
-
+    mant_mul = mant_mul.to(tl.uint16)
     # ==== exp add ====
-    exp_sum = exp_a + exp_b - 7  # bias = 7 for E4M3
+    exp_sum = exp_a + exp_b + 1  # bias = 15 for E5M10(float16) 1=-7-7+15
 
     # ==== normalize mantissa ====
-    mant_norm = mant_mul >> 4  # 简化，正常要看最高位 normalize
-    exp_sum = exp_sum + 1      # 确保 mantissa 是 1.xxxx
+    # pdb.set_trace()
+    tmp_bit = ((mant_mul&0x80)>>7)
+    exp_sum = exp_sum + tmp_bit      # 确保 mantissa 是 1.xxxx
+    mant_norm = mant_mul << (4 - tmp_bit)  # 将小数位移动到fp16的fraction的位置
 
-    # ==== pack back to FP8 ====
+    # ==== pack to FP16 ====
     # clip exp 和 mant
-    mant_norm = mant_norm & 0x7  # 保留 3 bit mantissa
-    exp_sum = tl.maximum(0, tl.minimum(exp_sum, 15))  # exp clip 到 4bit
+    mant_norm = mant_norm & 0x3FF  # 保留 10 bit mantissa
+    exp_sum = tl.maximum(0, tl.minimum(exp_sum, 31))  # exp clip 到 5bit
 
     sign = sign_a ^ sign_b
-    fp8_out = (sign << 7) | (exp_sum << 3) | mant_norm
+
+    sign = sign.to(tl.uint16)
+    exp_sum = exp_sum.to(tl.uint16)
+
+    fp16_out = (sign << 15) | (exp_sum << 10) | mant_norm
 
     # store
-    tl.store(c_ptr + offsets, fp8_out, mask=mask)
+    tl.store(c_ptr + offsets, fp16_out, mask=mask)
 
 def vecmul(a, b):
     # Check constraints.
     assert a.shape == b.shape, "Incompatible dimensions"
     assert a.is_contiguous(), "Matrix A must be contiguous"
-    c = torch.empty(a.shape, device=a.device, dtype=torch.uint8)
+    a = a.view(torch.uint8)
+    b = b.view(torch.uint8)
+    # print(f"a_viewasINT8={a}")
+    # print(f"b_viewasINT8={b}")
+    c = torch.empty(a.shape, device=a.device, dtype=torch.uint16)
     n_elements = a.numel()
     grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']), )
-    fp8_mul_kernel[grid](a, b, c, n_elements, 256)
-    c = c.view(torch.float8_e4m3fn)
+    fp8_mul_sim_kernel[grid](a, b, c, n_elements, 256)
+    # print(f"c_viewasINT16={c}")
+    c = c.view(torch.float16)
     return c
 
-# test fp8_mul_kernel accuracy
+# test fp8_mul_sim_kernel accuracy
 
 TORCH_HAS_FP8 = hasattr(torch, "float8_e4m3fn")
 if TORCH_HAS_FP8 and is_cuda():
     torch.manual_seed(0)
-    a = torch.randn((1), device='cuda', dtype=torch.float16)
-    b = torch.randn((1), device='cuda', dtype=torch.float16)
-    a_fp8 = a.to(torch.float8_e4m3fn)
-    b_fp8 = b.to(torch.float8_e4m3fn)
-    a_fp8 = a_fp8.view(torch.uint8)
-    b_fp8 = b_fp8.view(torch.uint8)
-    # print(triton.testing.do_bench(lambda: vecmul(a, b), warmup=10, rep=100))
-    triton_output = vecmul(a_fp8, b_fp8)
-    triton_output = triton_output.to(torch.float16)
-    torch_output = torch.mul(a, b)
-    print(f"triton_output_with_fp8_inputs={triton_output}")
-    print(f"torch_output={torch_output}")
-    if torch.allclose(triton_output, torch_output, atol=0.125, rtol=0):
-        print("✅ Triton and Torch match")
-    else:
-        print("❌ Triton and Torch differ")
+    a = torch.randn((256*4), device='cuda', dtype=torch.float16)
+    b = torch.randn((256*4), device='cuda', dtype=torch.float16)
+    a = a.to(torch.float8_e4m3fn)
+    b = b.to(torch.float8_e4m3fn)
+    # print(f"a={a}")
+    # print(f"b={b}")
+    print(triton.testing.do_bench(lambda: vecmul(a, b), warmup=10, rep=100))
+    triton_output = vecmul(a, b)
+    # triton_output = triton_output.to(torch.float16)
+    # assert a.dtype == torch.float8_e4m3fn
+    # assert b.dtype == torch.float8_e4m3fn
+    # torch_output = torch.mul(a.to(torch.float16), b.to(torch.float16))
+    # print(f"triton_output_with_fp8_inputs={triton_output}")
+    # print(f"torch_output={torch_output}")
+    # if torch.allclose(triton_output, torch_output, atol=0.125, rtol=0):
+    #     print("✅ Triton and Torch match")
+    # else:
+    #     print("❌ Triton and Torch differ")
 else:
     print("Skipping the test since torch.float8_e4m3fn is not available.")
